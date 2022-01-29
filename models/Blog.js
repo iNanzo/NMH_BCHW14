c5d840a2724eac031fed6fb4caa34d931d8e495d
Newton Hoang <newtonhoang99@gmail.com>
2022-01-28 20:09:32 -0800

    models filled

---
+++ models/Blog.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model{}
+
+Blog.init(
+    {
+        id:
+        {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title:
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        body:
+        {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id:
+        {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog_posts',
+    }
+);
+
+module.exports = Blog;
---
+++ models/Comment.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        id:
+        {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment:
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id:
+        {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+            }
+        },
+        blog_id:
+        {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Blog',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+)
+
+module.exports = Comment;
